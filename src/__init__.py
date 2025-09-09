16bf41bdcb64f9ad17fef9a83ab6edd778074004
Liting Zhou <liting.zhou@afry.com>
2025-09-09 14:50:57 +0200

    my first commit

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# This is a solution package for the Advent of Code 2024 challenges.
+# Each day's solution is organized in its own module within the 'days' package.
+
+__version__ = "0.0.1"
+__author__ = "Liting Zhou"
+__license__ = "MIT"
+
+
